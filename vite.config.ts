93305ce036f59301f543a759c528febb0ecaf7ae
Максим Бодэ <maksim.bode@tages.ru>
2025-03-10 13:24:28 +0300

    подготовка к ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'zettelkasten',
+  base: '/zettelkasten',
   plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
