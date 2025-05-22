8bc40ef8c2939f2dcd9ba8dfed4beb88e58a2886
bodemaxim <bodemaxim@gmail.com>
2025-05-22 13:24:58 +0300

    vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import checker from 'vite-plugin-checker'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/zettelkasten',
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    checker({
+      typescript: true,
+      vueTsc: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
